f068e83db4144d83006b81199132cccb0644be3e
van hieu <nobihaza115@gmail.com>
2026-09-20 13:55:32 +0700

    initial project

---
+++ backend/src/main/java/com/nhom10/tmdt/TmdtApplication.java
@@ -0,0 +1,13 @@
+package com.nhom10.tmdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdtApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nhom10/tmdt/TmdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom10.tmdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
